82bc513afcd199987d1dab9579d85bdbf557c96e
Adin Ashby <adin.ashby@gmail.com>
2024-02-21 11:47:41 -0500

    first commit

---
+++ firstprog.cpp
@@ -0,0 +1,13 @@
+// This is the first program that just writes out a simple message
+
+// Place your name here
+
+#include <iostream>	// needed to perform C++ I/O
+using namespace std;
+
+int main()
+{
+	cout << "Hello Vanier" << endl;
+
+	return 0;
+}
