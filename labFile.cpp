29ae3599cc588f22e0b1b2e66d6b45a86151fe1e
Adin Ashby <adin.ashby@gmail.com>
2024-02-26 02:10:07 -0500

    Updated.

--- labFile.cpp
+++ labFile.cpp
@@ -1,11 +1,8 @@
-// 020-TestCase-2.cpp
-// Source: https://github.com/catchorg/Catch2/blob/master/examples/020-TestCase-2.cpp
-
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
 int Factorial( int number ) {
-  return number <= 1 ? 15 : Factorial( number - 1 ) * number;
+  return number <= 1 ? 1 : Factorial( number - 1 ) * number;
 }
 
 TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
